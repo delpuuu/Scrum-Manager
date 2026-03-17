447d11e35b42cfa23866ffa1a423e975f0f8180c
delpuuu <soynicolasdelpino@gmail.com>
2026-03-17 13:16:28 -0300

    Solucion de database

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from '@prisma/config';
-import * as dotenv from 'dotenv';
-
-// Cargamos el archivo .env manualmente para que Prisma 7 lo vea
-dotenv.config();
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
+  // Prisma 7 CLI buscará automáticamente DATABASE_URL en el entorno o en el .env
   datasource: {
-    // Si sigue fallando, podés poner el string directo aquí para probar, 
-    // pero lo ideal es usar la variable de entorno:
     url: process.env.DATABASE_URL,
   },
 });
